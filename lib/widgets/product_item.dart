d9edb5ed2e594eda610bff21462535aa2a5fe3d3
Rakesh005 <srakeshswain005@gmail.com>
2022-06-02 14:46:03 +0530

    use Consummer instead of Provider.of

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -10,7 +10,7 @@ class ProductItem extends StatelessWidget {
   // ProductItem(this.id, this.title, this.imageurl);
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -28,13 +28,15 @@ class ProductItem extends StatelessWidget {
           ),
         ),
         footer: GridTileBar(
-          leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
-            color: Theme.of(context).accentColor,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+              color: Theme.of(context).accentColor,
+            ),
           ),
           backgroundColor: Colors.black87,
           title: Text(
