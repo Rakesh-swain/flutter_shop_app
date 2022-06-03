2e7f1478487ec2f2f53ea31953e8598b97398ee7
Rakesh005 <srakeshswain005@gmail.com>
2022-06-03 16:58:39 +0530

    add product detail data

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -10,6 +10,32 @@ class ProductDetailScreen extends StatelessWidget {
         .findById(productId);
     return Scaffold(
       appBar: AppBar(title: Text(loadedProduct.title)),
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(loadedProduct.imageurl, fit: BoxFit.cover),
+          ),
+          SizedBox(height: 10),
+          Text(
+            '${loadedProduct.price}',
+            style: TextStyle(
+                // color: Colors.grey,
+                fontSize: 20),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            child: Text(
+              loadedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
