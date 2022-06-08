f1842e2d32ddecb0895cf1f3eecc9c4bc33746b8
Rakesh005 <srakeshswain005@gmail.com>
2022-06-08 19:20:10 +0530

    handle authentication error

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String? _token;
@@ -11,13 +12,20 @@ class Auth with ChangeNotifier {
       String? email, String? password, String urlSegment) async {
     var url = Uri.parse(
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBUIC3UhUs0kBPKrxxJucLitnLugYVUBQk');
-    final response = await http.post(url,
-        body: json.encode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          }));
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String? email, String? password) async {
